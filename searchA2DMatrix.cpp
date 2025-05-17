0ea8eef7c72ec2b25921b50bf6dd853a6c845803
Ramisha Fariha Baki <74113323+Ramisha386@users.noreply.github.com>
2025-05-16 22:35:20 -0600

    Add files via upload

---
+++ searchA2DMatrix.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        if (m == 0) return false;
+        int n = matrix[0].size();
+        if (n == 0) return false;
+
+        int left = 0;
+        int right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    vector<vector<int>> matrix = {
+        {1, 3, 5, 7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 60}
+    };
+
+    int target = 16;
+
+    if (solution.searchMatrix(matrix, target)) {
+        cout << "Target " << target << " found in matrix." << endl;
+    } else {
+        cout << "Target " << target << " not found in matrix." << endl;
+    }
+
+    return 0;
+}
