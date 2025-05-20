faa4dca3bf1b174554680ca7b490ded6022273c5
Ramisha Fariha Baki <74113323+Ramisha386@users.noreply.github.com>
2025-05-19 21:20:37 -0600

    Add files via upload

---
+++ reverseLinkedList.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        while (curr != NULL) {
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
+
+// Helper function to print the linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+// Helper function to create a linked list from an array
+ListNode* createList(const vector<int>& values) {
+    if (values.empty()) return nullptr;
+    ListNode* head = new ListNode(values[0]);
+    ListNode* current = head;
+    for (size_t i = 1; i < values.size(); ++i) {
+        current->next = new ListNode(values[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+int main() {
+    // Create a linked list: 1 -> 2 -> 3 -> 4 -> 5
+    vector<int> values = {1, 2, 3, 4, 5};
+    ListNode* head = createList(values);
+
+    cout << "Original list: ";
+    printList(head);
+
+    // Reverse the linked list
+    Solution solution;
+    ListNode* reversedHead = solution.reverseList(head);
+
+    cout << "Reversed list: ";
+    printList(reversedHead);
+
+    return 0;
+}
