b43b8be9cb1ae6b7cea90ca3fab230a50fc1778d
Ramisha Fariha Baki <74113323+Ramisha386@users.noreply.github.com>
2025-05-21 18:38:09 -0600

    Add files via upload

--- 3sums.cpp
+++ 3sums.cpp
@@ -1,60 +1,55 @@
 #include <iostream>
 #include <vector>
-#include <set>
 #include <algorithm>
-#include <fstream> // For atexit use
 
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> v;
-        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            int l = i + 1, r = n - 1;
+        vector<vector<int>> result;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1;
+            int r = n - 1;
+
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
-                if (sum < 0) {
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[l], nums[r]});
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
                     l++;
+                    r--;
                 } else if (sum > 0) {
                     r--;
                 } else {
-                    v.insert({nums[i], nums[l], nums[r]});
                     l++;
-                    r--;
                 }
             }
         }
 
-        for (auto it : v) {
-            ans.push_back(vector<int>(it.begin(), it.end()));
-        }
-
-        return ans;
+        return result;
     }
 };
 
-// For whatever purpose this was originally used
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
-
 int main() {
+    vector<int> nums = {0, 0, 0, 0};  // You can change this input for other test cases
     Solution sol;
+    vector<vector<int>> triplets = sol.threeSum(nums);
 
-    vector<int> nums = {-1, 0, 1, 2, -1, -4};
-    vector<vector<int>> result = sol.threeSum(nums);
-
-    cout << "Triplets that sum to 0:\n";
-    for (const auto& triplet : result) {
+    cout << "Triplets that sum to zero:" << endl;
+    for (const auto& triplet : triplets) {
         cout << "[";
         for (size_t i = 0; i < triplet.size(); ++i) {
             cout << triplet[i];
             if (i != triplet.size() - 1) cout << ", ";
         }
-        cout << "]\n";
+        cout << "]" << endl;
     }
 
     return 0;
