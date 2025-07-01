08e51ff589c640ad40afc38967b33939060fbf4c
Ramisha Fariha Baki <74113323+Ramisha386@users.noreply.github.com>
2025-07-01 15:34:36 -0600

    Add files via upload

---
+++ dailyTemperature.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> answer(n, 0);
+        stack<int> s; // store indices
+
+        for (int i = 0; i < n; ++i) {
+            while (!s.empty() && temperatures[s.top()] < temperatures[i]) {
+                int prevIndex = s.top();
+                s.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+            s.push(i);
+        }
+        return answer;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+
+    vector<int> result = sol.dailyTemperatures(temperatures);
+
+    cout << "Result: ";
+    for (int days : result) {
+        cout << days << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
